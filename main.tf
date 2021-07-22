9daa3611476994289ec45ecd00e906cfe9a5939a
wullub <william@automationlogic.com>
2021-07-22 16:26:24 +0100

    Use uk time (#7)
    * use uk time
    
    use uk time
    
    * ..
    
    * fix start time
    
    fix start time

--- main.tf
+++ main.tf
@@ -16,11 +16,11 @@ module "automation_account" {
   schedules         = lookup(each.value, "schedules", {
     "weekdays 7am" = {
       week_days     = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"],
-      start_time    = "2021-07-19T07:00:00Z" # annoyingly this needs to be 5 mins ahead of the first deployment and the time that you want the job to run
+      start_time    = "2021-07-23T07:00:00Z" # annoyingly this needs to be 5 mins ahead of the first deployment and the time that you want the job to run
     },
     "weekdays 7pm" = {
       week_days     = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"],
-      start_time    = "2021-07-16T19:00:00Z"
+      start_time    = "2021-07-22T19:00:00Z"
     }
   })
   job_schedules     = each.value.job_schedules

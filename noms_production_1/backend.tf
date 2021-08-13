86293864b7f0ce6f620eedca94205e0f587fc70c
W <willg258@hotmail.co.uk>
2021-08-13 13:59:24 +0100

    add other subscriptions
    add other subscriptions

--- noms_production_1/backend.tf
+++ noms_production_1/backend.tf
@@ -1,1 +1,10 @@
-../backend.tf
+terraform {
+  backend "azurerm" {
+    subscription_id      = "b1f3cebb-4988-4ff9-9259-f02ad7744fcb" # NOMS Dev & Test Environments
+    tenant_id            = "747381f4-e81f-4a43-bf68-ced6a1e14edf"
+    resource_group_name  = "dso-terraform-state"
+    storage_account_name = "dsoautomationaccounts"
+    container_name       = "automation-accounts-noms-production-1-tfstate"
+    key                  = "terraform.tfstate"
+  }
+}

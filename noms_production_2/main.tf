13a257680d6dfebedde5869722685f3124610574
Jonathan Quinn <jonathan.quinn@digital.justice.gov.uk>
2021-11-23 09:17:06 +0000

    add a second schedule for DSO-1143

---
+++ noms_production_2/main.tf
@@ -0,0 +1,27 @@
+provider "azurerm" {
+  features {}
+}
+
+##
+# automation
+
+module "automation_account" {
+  source            = "../modules/automation_account"
+  for_each          = var.automation_accounts
+  resource_group    = each.key
+  script_templates  = lookup(each.value, "script_templates", [
+    "start-vms", 
+    "stop-vms"
+  ])
+  schedules         = lookup(each.value, "schedules", var.schedules)
+  job_schedules     = lookup(each.value, "job_schedules", [
+    {
+      schedule = "saturday 7am"
+      script   = "stop-vms"
+    },
+    {
+      schedule = "monday 7am"
+      script   = "start-vms"
+    }
+  ])
+}

2c1f6db83edbb40fa51e9b8b5c6ff0442c56ce8f
James Borwick <james.borwick@icloud.com>
2021-12-13 12:37:07 +0000

    Added missing variable declaration

--- modules/automation_account/variables.tf
+++ modules/automation_account/variables.tf
@@ -2,3 +2,4 @@ variable "resource_group" { type = string }
 variable "script_templates" { type = list(any) }
 variable "schedules" { type = map(any) }
 variable "job_schedules" { type = list(any) }
+variable "delay_between_groups" { type = number }

1da049c9a5a450fa957de090c944c87177e71e01
Dominic Robinson <dominic.robinson@digital.justice.gov.uk>
2022-04-08 14:06:08 +0100

    DSO-884: migrate state to shared storage account

--- noms_dev_&_test_environments/backend.tf
+++ noms_dev_&_test_environments/backend.tf
@@ -3,8 +3,8 @@ terraform {
     subscription_id      = "b1f3cebb-4988-4ff9-9259-f02ad7744fcb" # NOMS Dev & Test Environments
     tenant_id            = "747381f4-e81f-4a43-bf68-ced6a1e14edf"
     resource_group_name  = "dso-terraform-state"
-    storage_account_name = "dsoautomationaccounts"
-    container_name       = "automation-accounts-tfstate"
-    key                  = "terraform.tfstate"
+    storage_account_name = "dsotfstatedevtest"
+    container_name       = "dso-azure-automation-accounts"
+    key                  = "noms_dev_&_test_environments/terraform.v1.tfstate"
   }
 }
--- noms_production_2/backend.tf
+++ noms_production_2/backend.tf
@@ -1,10 +1,10 @@
 terraform {
   backend "azurerm" {
-    subscription_id      = "b1f3cebb-4988-4ff9-9259-f02ad7744fcb" # NOMS Dev & Test Environments
+    subscription_id      = "1d95dcda-65b2-4273-81df-eb979c6b547b"
     tenant_id            = "747381f4-e81f-4a43-bf68-ced6a1e14edf"
     resource_group_name  = "dso-terraform-state"
-    storage_account_name = "dsoautomationaccounts"
-    container_name       = "automation-accounts-noms-production-2-tfstate"
-    key                  = "terraform.tfstate"
+    storage_account_name = "dsotfstateprod"
+    container_name       = "dso-azure-automation-accounts"
+    key                  = "noms_production_2/terraform.v1.tfstate"
   }
 }
